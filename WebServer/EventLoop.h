98f12fc21e1e329d9d5b388cc7cb0b1b74430419
JxinHe <40479291+JxinHe@users.noreply.github.com>
2019-02-27 21:16:55 +0800

    Update EventLoop.h

--- WebServer/EventLoop.h
+++ WebServer/EventLoop.h
@@ -25,27 +25,27 @@ public:
     void quit();
     void runInLoop(Functor&& cb);
     void queueInLoop(Functor&& cb);
-    bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
+    bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }/判断是否在本IO线程
     void assertInLoopThread()
     {
-        assert(isInLoopThread());
+        assert(isInLoopThread());//同上把？？
     }
-    void shutdown(shared_ptr<Channel> channel)
+    void shutdown(shared_ptr<Channel> channel)//半关闭
     {
         shutDownWR(channel->getFd());
     }
-    void removeFromPoller(shared_ptr<Channel> channel)
+    void removeFromPoller(shared_ptr<Channel> channel)//删除已执行回调的活跃的fd??
     {
         //shutDownWR(channel->getFd());
         poller_->epoll_del(channel);
     }
     void updatePoller(shared_ptr<Channel> channel, int timeout = 0)
     {
-        poller_->epoll_mod(channel, timeout);
+        poller_->epoll_mod(channel, timeout);//修改fd??
     }
     void addToPoller(shared_ptr<Channel> channel, int timeout = 0)
     {
-        poller_->epoll_add(channel, timeout);
+        poller_->epoll_add(channel, timeout);//添加fd
     }
     
 private:
